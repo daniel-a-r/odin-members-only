9710e2a16106318b5ab1686face4712ec3dd3360
Daniel Aguilar-Rodriguez <daniel.a-r@outlook.com>
2025-04-15 01:33:23 -0400

    fix database connection issue

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,3 +1,3 @@
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASS}@${process.env.PG_HOST}:${process.env.PG_PORT}/users`;
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASS}@${process.env.PG_HOST}:${process.env.PG_PORT}/odin_members_only`;
 
 export default connectionString;
