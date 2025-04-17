2e9ab096fd5381840f9c9d9f8bd42998518152ad
Daniel Aguilar-Rodriguez <26808382+daniel-a-r@users.noreply.github.com>
2025-04-17 01:38:45 -0400

    update database init file

--- db/initdb.js
+++ db/initdb.js
@@ -5,7 +5,7 @@ import bcrypt from 'bcryptjs';
 
 const { Client } = pg;
 
-const SQL = `
+const CREATE_USERS_TABLE = `
   DROP TABLE IF EXISTS users;
 
   CREATE TABLE IF NOT EXISTS users (
@@ -17,6 +17,18 @@ const SQL = `
   );
 `;
 
+const CREATE_MESSAGES_TABLE = `
+  DROP TABLE IF EXISTS messages;
+
+  CREATE TABLE IF NOT EXISTS messages (
+    id SERIAL PRIMARY KEY,
+    subject VARCHAR NOT NULL,
+    content TEXT NOT NULL,
+    timestamp TIMESTAMPTZ NOT NULL,
+    user_id UUID REFERENCES users
+  );
+`;
+
 const main = async () => {
   console.log('Creating table...');
   const client = new Client({
@@ -24,13 +36,15 @@ const main = async () => {
   });
 
   await client.connect();
-  await client.query(SQL);
+  await client.query(CREATE_USERS_TABLE);
 
   const hashedPassword = await bcrypt.hash(process.env.ADMIN_PW, 10);
   await client.query(
     'INSERT INTO users (username,  password, member, admin) VALUES ($1, $2, $3, $4)',
     ['danny', hashedPassword, true, true],
   );
+
+  await client.query(CREATE_MESSAGES_TABLE);
   await client.end();
   console.log('Done');
 };
