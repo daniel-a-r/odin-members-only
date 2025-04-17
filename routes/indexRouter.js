4ec5fe925ec5edcb79f8d9b41e37fa6f1bff408c
Daniel Aguilar-Rodriguez <26808382+daniel-a-r@users.noreply.github.com>
2025-04-16 23:17:28 -0400

    add validation to login

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -31,6 +31,14 @@ const validateUserSignUp = [
   body('confirmPassword').passwordsMatch(),
 ];
 
+const validateUserLogin = [
+  body('username')
+    .trim()
+    .toLowerCase()
+    .isAlphanumeric()
+    .withMessage('Invalid username'),
+];
+
 const indexRouter = Router();
 
 indexRouter.get('/', (req, res) => {
@@ -63,10 +71,26 @@ indexRouter
 indexRouter
   .route('/login')
   .get((req, res) => {
-    // console.log(req.session);
-    res.render('login', { title: 'Login' });
+    res.render('login', { title: 'Login', errors: req.session.messages });
   })
   .post(
+    validateUserLogin,
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        // extract error messages from validation errors objects
+        const errorMessages = errors.array().map((err) => err.msg);
+        return res
+          .status(400)
+          .render('login', { title: 'Login', errors: errorMessages });
+      }
+      next();
+    },
+    (req, res, next) => {
+      // clear error messages before authenticating
+      req.session.messages = [];
+      next();
+    },
     passport.authenticate('local', {
       successRedirect: '/',
       failureRedirect: '/login',
