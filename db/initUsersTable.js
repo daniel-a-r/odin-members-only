f0b57bf1d1e3b006e5b6edb6583e0ad55ea45073
Daniel Aguilar-Rodriguez <26808382+daniel-a-r@users.noreply.github.com>
2025-04-16 22:24:58 -0400

    rename and create default user

--- db/initUsersTable.js
+++ db/initUsersTable.js
@@ -1,14 +1,19 @@
 import '@dotenvx/dotenvx/config';
 import pg from 'pg';
 import { connectionString } from '../config/dbConfig.js';
+import bcrypt from 'bcryptjs';
 
 const { Client } = pg;
 
 const SQL = `
+  DROP TABLE IF EXISTS users;
+
   CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR,
-    password VARCHAR
+    password VARCHAR,
+    member BOOLEAN,
+    admin BOOLEAN
   );
 `;
 
@@ -17,8 +22,15 @@ const main = async () => {
   const client = new Client({
     connectionString,
   });
+
   await client.connect();
   await client.query(SQL);
+
+  const hashedPassword = await bcrypt.hash(process.env.ADMIN_PW, 10);
+  await client.query(
+    'INSERT INTO users (username,  password, member, admin) VALUES ($1, $2, $3, $4)',
+    ['danny', hashedPassword, true, true],
+  );
   await client.end();
   console.log('Done');
 };
